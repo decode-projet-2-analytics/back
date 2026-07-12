dc63e26a6cc3745587f4667e72c03f26fcb851a8
Ilian Igoudjil <ilianigoudjil@gmail.com>
2026-07-12 20:41:00 +0200

    fix clicks et autre

--- models/widget.js
+++ models/widget.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const connection = require('../lib/db');
 
-const WIDGET_TYPES = ['kpi', 'timeseries', 'heatmap', 'mouse_heatmap'];
+const WIDGET_TYPES = ['kpi', 'timeseries', 'heatmap', 'mouse_heatmap', 'breakdown', 'scroll_depth'];
 const WIDGET_METRICS = ['count', 'rate'];
 
 const defaultConfig = {
